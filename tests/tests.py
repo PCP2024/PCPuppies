287bdc7a93ceae9007d5329bb2a2f5f21e79f618
bojobrader <bonnie.brader@gmx.net>
2024-04-30 18:01:51 +0200

    first test

--- tests/tests.py
+++ tests/tests.py
@@ -6,8 +6,8 @@ class Test_LoadFunctions(unittest.TestCase):
     """ Simple functionality tests. """
 
     def test_load_image_as_PILimage(self):        
-        image = load_image_as_PILimage('./demodata/johann.jpg')
-        self.assertEqual(image.size, (3840, 2160), 'Image size is not correct')
+        image = load_image_as_PILimage('.\demodata\example_pic.jpg')
+        self.assertEqual(image.size, (690, 786), 'Image size is not correct')
         
 
 if __name__ == '__main__':
