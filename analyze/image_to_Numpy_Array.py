d01c71176cc028c67826aad5cc6e0c24dcdde2af
bojobrader <bonnie.brader@gmx.net>
2024-05-11 11:10:33 +0200

    changelog beginning

---
+++ analyze/image_to_Numpy_Array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def imageToNumpyArray(image):
+    '''
+    convert image to numpy array
+    :param image: image to convert
+    :return: numpy array
+    '''
+    return np.array(image)
