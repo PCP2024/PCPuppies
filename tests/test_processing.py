fc3dc95ddb0376b6b8d78992a57be76891c6770e
SamGomez170 <103229101+SamGomez170@users.noreply.github.com>
2024-05-11 12:12:28 -1100

    test edge

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,10 +5,13 @@ from processing import processing_fun
 
 class ProcessingTestCase(unittest.TestCase):
     def setUp(self):
-      self.original_image = image_loader.load_image_as_PILimage('C:/Users/gomar/OneDrive/Escritorio/PCPuppies/PCPuppies/1.jpeg')
+      self.original_image = image_loader.load_image_as_PILimage('.\demodata\example_pic.jpg')
       self.rotated = processing_fun.rotate(self.original_image, 90)
       self.mirrored = processing_fun.mirror(self.original_image)
       self.blured = processing_fun.blur(self.original_image)
+      self.edge_enhance = processing_fun.edge_enhance(self.original_image)
+      self.edges = processing_fun.find_edges(self.original_image)
+      
 
     def test_rotate_image(self):
       self.assertEqual(self.rotated.size, self.original_image.size, 'The image is not rotated')
@@ -17,6 +20,11 @@ class ProcessingTestCase(unittest.TestCase):
       mirrored = np.array(self.mirrored)
       image = np.array(self.original_image)
       self.assertEqual(np.array_equal(np.flip(image, axis=1), mirrored), True, 'The image is not mirrored')
+      
+    def test_edge_enhance(self):
+      self.assertEqual(self.edge_enhance.size, self.original_image.size, "The image size has changed after applying edge enhance filter")
+    
+       
   
 if __name__ == '__main__':
     unittest.main()   
