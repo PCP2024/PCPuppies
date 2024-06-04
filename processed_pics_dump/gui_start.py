f3b67a7d075f1612407d1ba5fe0522426058ef5d
bojobrader <bonnie.brader@gmx.net>
2024-06-04 15:59:45 +0200

    gui start

---
+++ processed_pics_dump/gui_start.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PC Puppies")
+
+        button = QPushButton("Click me")
+        button.clicked.connect(self.button_click)
+
+        self.setCentralWidget(button)
+
+    def button_click(self):
+        print("Button clicked")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
